cba472cacb94a38396de1dfb044612bb94473529
brice de la serviere <alicesantossilva345@gmail.com>
2024-10-11 09:47:54 -0300

    commit inicial

---
+++ stack_queue/main.py
@@ -0,0 +1,32 @@
+# stack
+
+pilha = []
+
+def inserir(x):
+    pilha.insert(0, x)
+
+def remover():
+    pilha.pop(0)
+
+inserir('a')
+inserir('b')
+inserir('c')
+inserir('d')
+
+print(pilha)
+
+# queue
+
+fila = []
+
+def insert(x):
+    fila.append(x)
+
+def remove()
+    fila.pop()
+
+insert(1)
+insert(2)
+insert(3)
+
+print(fila)
